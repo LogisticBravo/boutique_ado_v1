089e51f1984176822922613a85e1aa5f57aa42fc
LogisticBravo <jay.jbradley@gmail.com>
2021-08-09 21:31:47 +0000

    setup all auth

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -19,5 +19,6 @@ import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
 ]
