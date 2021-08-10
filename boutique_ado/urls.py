6ad7191bfaf5c6aab2ac8758fe11c51e28d188a9
LogisticBravo <jay.jbradley@gmail.com>
2021-08-10 20:24:31 +0000

    Add products view and template

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('products', include('products.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
