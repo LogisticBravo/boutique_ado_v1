86d44520fc59fdef8466bb0836beddc09cc4d7db
LogisticBravo <jay.jbradley@gmail.com>
2021-09-02 19:24:09 +0000

    Add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JVLAUFlFfTxzZDIFaVv8Z2FFCniDH9uwvveVAYMgNGkOasUUsNOsOYC98ygt9lrOBIGxOSCpFgAv5ugKbKagNZ700szzHd5YM',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
